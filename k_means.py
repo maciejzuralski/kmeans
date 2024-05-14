22a99707acadb480b8d415677ace4de224cf5dc9
Maciej Zuralski <maciejzuralski13@icloud.com>
2024-05-14 11:16:46 +0200

    assign_to_cluster and update_centroids

--- k_means.py
+++ k_means.py
@@ -1,42 +1,65 @@
 import numpy as np
 import math
 
-def initialize_centroids_forgy(data, k):
-    randomCentroidsIndex = np.arange(data.shape[0])
-    randomCentroidsIndex = np.random.choice(randomCentroidsIndex, k, replace=False)
-    randomCentroids = data[randomCentroidsIndex]
-    return randomCentroids
-
-def initialize_centroids_kmeans_pp(data, k):
-    def distanceBeatweenNodes(x, y):
+def distanceBeatweenPoints(x, y):
         distance = 0
         for i in range(len(x)):
             distance += (x[i] - y[i])**2  
             
         return math.sqrt(distance)
-    
+
+def initialize_centroids_forgy(data, k):
+    randomCentroidsIndex = np.arange(data.shape[0])
+    randomCentroidsIndex = np.random.choice(randomCentroidsIndex, k, replace=False)
+    randomCentroids = data[randomCentroidsIndex]
+    return np.array(randomCentroids)
+
+def initialize_centroids_kmeans_pp(data, k):
     centroids = []
     centroids.append(data[np.random.randint(0, data.shape[0])])
-    for i in range(k - 1):
+    
+    for _ in range(k - 1):
         furthestDistance = 0
-        for node in data:
+        for point in data:
             distance = 0
             for centroid in centroids:
-                distance += distanceBeatweenNodes(centroid, node)
+                distance += distanceBeatweenPoints(centroid, point)
             if furthestDistance < distance:
                 furthestDistance = distance
-                furthestNode = node
-        centroids.append(furthestNode)
+                furthestpoint = point
+        centroids.append(furthestpoint)
         
-    return centroids
+    return np.array(centroids)
 
-def assign_to_cluster(data, centroid):
+def assign_to_cluster(data, centroids):
     # TODO find the closest cluster for each data point
-    return None
+    assignments = []
+    for point in data:
+        nearestDistance = np.inf
+        nearestDistanceCentroid = 0
+        for i, centroid in enumerate(centroids):
+            distance = distanceBeatweenPoints(point, centroid)
+            if nearestDistance > distance:
+                nearestDistance = distance
+                nearestDistanceCentroid = i
+        
+        assignments.append(int(nearestDistanceCentroid))
+    
+    return np.array(assignments)
 
-def update_centroids(data, assignments):
+def update_centroids(data, assignments, centroids):
     # TODO find new centroids based on the assignments
-    return None
+    assigmentsPoints = [[] for _ in range(max(assignments) + 1)]
+    for i in range(len(assignments)):
+        assigmentsPoints[assignments[i]].append(data[i])
+        
+    newCentroids = []
+    for points in assigmentsPoints:
+        if len(points) > 0:
+            newCentroids.append(np.mean(points, 0))
+        else:
+            newCentroids.append(centroids[len(newCentroids)])
+    return np.array(newCentroids)
 
 def mean_intra_distance(data, assignments, centroids):
     return np.sqrt(np.sum((data - centroids[assignments, :])**2))
@@ -52,7 +75,7 @@ def k_means(data, num_centroids, kmeansplusplus= False):
     assignments  = assign_to_cluster(data, centroids)
     for i in range(100): # max number of iteration = 100
         print(f"Intra distance after {i} iterations: {mean_intra_distance(data, assignments, centroids)}")
-        centroids = update_centroids(data, assignments)
+        centroids = update_centroids(data, assignments, centroids)
         new_assignments = assign_to_cluster(data, centroids)
         if np.all(new_assignments == assignments): # stop if nothing changed
             break
